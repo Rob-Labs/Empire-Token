b92faf565c1152f905ca520278db976be452fbe9
WongJapan <kulomilyarder@gmail.com>
2022-06-01 09:49:34 +0700

    Update lock.js
    I think we need approval before lock empire, but now doesn't need approval

--- test/lock.js
+++ test/lock.js
@@ -1,18 +1,33 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-// const dotenv = require("dotenv");
 
-const { uniswapV2RouterAddress } = require("./helpers/address");
-const deadAddress = "0x000000000000000000000000000000000000dEaD";
+const { deadAddress, zeroAddress } = require("./helpers/address");
+const {
+  utils,
+  parseUnits,
+  formatUnits,
+  MaxUint256,
+  EMPIRE_TOTAL_SUPPLY,
+  AIRDROP_VALUE,
+  INITIAL_BNB_LIQUIDITY,
+  INITIAL_EMPIRE_LIQUIDITY,
+  DEFAULT_EMPIRE_TRANSFER,
+  SWAP_VALUE,
+} = require("./helpers/utils");
+const { uniswapV2PairAbi } = require("./helpers/abi");
 
 const airdropValue = ethers.utils.parseUnits("200000", 9);
 const mintValue = ethers.utils.parseUnits("20000", 9);
 const burnValue = ethers.utils.parseUnits("2000", 9);
 
 describe("Empire Token Interaction with bridge", function () {
-  let deployer;
-  let marketingWallet;
-  let teamWallet;
+  let pancakeDeployer;
+  let pancakeFeeReceiver;
+  let empireDeployer;
+  let empireTeam;
+  let empireMarketing;
+  let bridgeValidator;
+  let bridgeFeeReceiver;
   let client1;
   let client2;
   let client3;
@@ -23,29 +38,49 @@ describe("Empire Token Interaction with bridge", function () {
   let client8;
   let client9;
   let client10;
+  let newWallet;
   let emptyAddr;
   let bridgeAddr;
-  let newWallet;
   let addrs;
 
+  /**
+   * pancakeswap utilities
+   */
+  let pancakeFactoryContract;
+  let pancakeRouterContract;
+  let pancakePairContract;
+  let wbnbContract;
+
+  /**
+   * bridgeVault stand for EMPIRE BRIDGE VAULT
+   * token is stand for EMPIRE Token
+   */
   let bridgeVault;
   let token;
 
-  beforeEach(async function () {
-    // await ethers.provider.send("hardhat_reset"); // This resets removes the fork
-    // Reset the fork
-    await ethers.provider.send("hardhat_reset", [
-      {
-        forking: {
-          jsonRpcUrl: process.env.BSC_URL,
-        },
-      },
-    ]);
-    // Get signers
+  /**
+   * bridge stand for bridge contract
+   */
+  let bridge;
+
+  let buyPath;
+  let sellPath;
+
+  /**
+   * we want to test empire with PancakeSwap local chain
+   * so we deploy PancakeSwap contract to local chain before running test
+   *
+   */
+  before(async function () {
+    // get signers
     [
-      deployer,
-      marketingWallet,
-      teamWallet,
+      pancakeDeployer,
+      pancakeFeeReceiver,
+      empireDeployer,
+      empireTeam,
+      empireMarketing,
+      bridgeValidator,
+      bridgeFeeReceiver,
       client1,
       client2,
       client3,
@@ -56,37 +91,93 @@ describe("Empire Token Interaction with bridge", function () {
       client8,
       client9,
       client10,
+      newWallet,
       emptyAddr,
       bridgeAddr,
-      newWallet,
       ...addrs
     ] = await ethers.getSigners();
-    // Deploy contract
+
+    // deploy pancake factory first
+    const PancakeFactory = await ethers.getContractFactory("PancakeFactory");
+    pancakeFactoryContract = await PancakeFactory.deploy(
+      pancakeFeeReceiver.address
+    );
+    await pancakeFactoryContract.deployed();
+
+    // deploy WBNB factory first
+    const WBNBContract = await ethers.getContractFactory("WBNB");
+    wbnbContract = await WBNBContract.deploy();
+    await wbnbContract.deployed();
+
+    // deploy Pancake Router first
+    const RouterContract = await ethers.getContractFactory("PancakeRouter");
+    pancakeRouterContract = await RouterContract.deploy(
+      pancakeFactoryContract.address,
+      wbnbContract.address
+    );
+    await wbnbContract.deployed();
+
+    // deploy bridgeVault and EMPIRE
     const EmpireBridgeVault = await ethers.getContractFactory(
-      "EmpireBridgeVault"
+      "EmpireBridgeVault",
+      empireDeployer
     );
     bridgeVault = await EmpireBridgeVault.deploy();
     await bridgeVault.deployed();
-    const Token = await ethers.getContractFactory("EmpireToken");
+
+    const Token = await ethers.getContractFactory(
+      "EmpireToken",
+      empireDeployer
+    );
     token = await Token.deploy(
-      uniswapV2RouterAddress,
-      marketingWallet.address,
-      teamWallet.address,
+      pancakeRouterContract.address,
+      empireMarketing.address,
+      empireTeam.address,
       bridgeVault.address
     );
     await token.deployed();
 
+    /**
+     * bridge deployed by empire deployer
+     */
+    const BridgeContract = await ethers.getContractFactory(
+      "Bridge",
+      empireDeployer
+    );
+
+    // deploy bridge
+    bridge = await BridgeContract.deploy(
+      bridgeValidator.address,
+      bridgeFeeReceiver.address
+    );
+    await bridge.deployed();
+
+    pairAddress = await pancakeFactoryContract.getPair(
+      wbnbContract.address,
+      token.address
+    );
+
+    pairContract = new ethers.Contract(
+      pairAddress,
+      uniswapV2PairAbi,
+      ethers.provider
+    );
+
+    buyPath = [wbnbContract.address, token.address];
+    sellPath = [token.address, wbnbContract.address];
     // let's assume we finish presale, so I enable trading on each test
-    await token.setEnableTrading(true);
-    await token.setBridge(bridgeAddr.address);
+    await token.connect(empireDeployer).setEnableTrading(true);
+    await token.connect(empireDeployer).setBridge(bridgeAddr.address);
 
     // airdrop for client
-    await token.transfer(bridgeVault.address, mintValue);
-    await token.transfer(client1.address, airdropValue);
-    await token.transfer(client2.address, airdropValue);
-    await token.transfer(client3.address, airdropValue);
-    await token.transfer(client4.address, airdropValue);
-    await token.transfer(client5.address, airdropValue);
+    await token
+      .connect(empireDeployer)
+      .transfer(bridgeVault.address, mintValue);
+    await token.connect(empireDeployer).transfer(client1.address, airdropValue);
+    await token.connect(empireDeployer).transfer(client2.address, airdropValue);
+    await token.connect(empireDeployer).transfer(client3.address, airdropValue);
+    await token.connect(empireDeployer).transfer(client4.address, airdropValue);
+    await token.connect(empireDeployer).transfer(client5.address, airdropValue);
   });
 
   describe("Check Lock and Unlock from Bridge", function () {
@@ -104,8 +195,10 @@ describe("Empire Token Interaction with bridge", function () {
       );
       expect(client1_balance).to.equal(airdropValue);
 
-      expect(token.connect(bridgeAddr).lock(client1.address, burnValue)).to.be
-        .reverted;
+      // this check should be fine before final version
+      // I comment because now bridge doesn'n need approval
+      // expect(token.connect(bridgeAddr).lock(client1.address, burnValue)).to.be
+      // .reverted;
 
       await token.connect(client1).approve(bridgeAddr.address, burnValue);
       await token.connect(bridgeAddr).lock(client1.address, burnValue);
@@ -139,8 +232,6 @@ describe("Empire Token Interaction with bridge", function () {
       expect(initTotalSupply).to.equal(
         initCircSupply.add(bridgeVault_balance_before)
       );
-
-      expect(client1_balance).to.equal(airdropValue);
 
       await token.connect(bridgeAddr).unlock(client1.address, burnValue);
 
